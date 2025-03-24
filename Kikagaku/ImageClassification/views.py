2eea3da946c40d59ad171f537b47171ebcd7ec8e
tuk19 <t.waki001@gmail.com>
2025-03-25 00:25:08 +0900

    update: jpegとpngファイル以外はエラーを表示するように変更

--- Kikagaku/ImageClassification/views.py
+++ Kikagaku/ImageClassification/views.py
@@ -1,16 +1,37 @@
 from django.shortcuts import render
+from django.core.exceptions import ValidationError
+from django.http import HttpResponse
 from .forms import ImageUploadform
 
 def index(request):
     if request.method == "POST":
+        context = {}
         form = ImageUploadform(request.POST, request.FILES)
         if form.is_valid():
             image = request.FILES['image']
+            # 画像の MIME タイプを取得
+            image_type = image.content_type
+            if image_type not in ['image/jpeg', 'image/png']:
+                form = ImageUploadform()
+                context = {
+                    'form': form,
+                    'error_message': 'この画像はアップロードできません。JPEG または PNG 形式の画像のみアップロード可能です',
+                }
+
+                return render(request, 'imageclassification/index.html', context)
+
             context = {
                 'form': form,
+                'image_type': image_type,
                 'image': image
             }
             return render(request, 'imageclassification/index.html', context)
+        else:
+            form = ImageUploadform()
+            context = {
+                'form': form,
+                'error_message': 'このファイルはアップロードできません。JPEG または PNG 形式の画像のみアップロード可能です',
+            }
     else:
         form = ImageUploadform()
         context = {
