23ccb2b2461d8cce4b94da13a64325f3a972c0ff
tuk19 <t.waki001@gmail.com>
2025-03-24 23:45:26 +0900

    add: 画像アップロード機能の追加

--- Kikagaku/ImageClassification/views.py
+++ Kikagaku/ImageClassification/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from .forms import ImageUploadform
 
 def index(request):
-    return render(request, 'imageclassification/index.html')
+    if request.method == "POST":
+        form = ImageUploadform(request.POST, request.FILES)
+        if form.is_valid():
+            image = request.FILES['image']
+            context = {
+                'form': form,
+                'image': image
+            }
+            return render(request, 'imageclassification/index.html', context)
+    else:
+        form = ImageUploadform()
+        context = {
+            'form': form,
+        }
+
+    return render(request, 'imageclassification/index.html', context)
