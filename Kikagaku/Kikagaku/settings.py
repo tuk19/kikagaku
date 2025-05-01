696507890a27144c01ca02c3b1a59274553a0c8f
tuk19 <t.waki001@gmail.com>
2025-05-01 19:15:44 +0900

    fix: settings.pyを修正、django-sassを削除

--- Kikagaku/Kikagaku/settings.py
+++ Kikagaku/Kikagaku/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_sass',   # django-sassを有効化
     'home',
     'ImageClassification',
     'PoseAnalysis',
@@ -125,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
