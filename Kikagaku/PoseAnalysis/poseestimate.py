f52a921c6b94c2cd0a7d1e0f71cee3c072e5c479
tuk19 <t.waki001@gmail.com>
2025-05-15 17:25:50 +0900

    fix: yolov8n-pose.ptをアプリフォルダに配置

--- Kikagaku/PoseAnalysis/poseestimate.py
+++ Kikagaku/PoseAnalysis/poseestimate.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from ultralytics import YOLO
 
 import subprocess
+import os
 
 def convert_to_h264(input_path, output_path):
     subprocess.run([
@@ -14,7 +15,9 @@ def convert_to_h264(input_path, output_path):
 
 
 def estimate_video_pose(input_path, output_path, fps_rate):
-    model = YOLO('yolov8n-pose.pt')
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    MODEL_PATH = os.path.join(BASE_DIR, 'yolov8n-pose.pt')
+    model = YOLO(MODEL_PATH)
     cap = cv2.VideoCapture(input_path)
     fps = cap.get(cv2.CAP_PROP_FPS) * fps_rate
     w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -36,7 +39,9 @@ def estimate_video_pose(input_path, output_path, fps_rate):
     out.release()
 
 def estimate_image_pose(input_path):
-    model = YOLO('yolov8n-pose.pt')
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    MODEL_PATH = os.path.join(BASE_DIR, 'yolov8n-pose.pt')
+    model = YOLO(MODEL_PATH)
     results = model(input_path)
     img = results[0].plot()
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
